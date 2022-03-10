14d051b822ab46f7cb59c19f940eca2236d1d3b8
Marcel Veselka <marcel.veselka@tesena.com>
2022-03-10 21:20:01 +0100

    small improvements

--- js/script.js
+++ js/script.js
@@ -152,16 +152,6 @@
       ]
     });
 
-
-    // scroll
-    // $('.scrollTo').on('click', function (e) {
-    //   e.preventDefault();
-    //   var target = $(this).attr('href');
-    //   $('html, body').animate({
-    //     scrollTop: ($(target).offset().top)
-    //   }, 500);
-    // });
-
   });
 
 })(jQuery);
