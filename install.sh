70a0cbeeeae991b9a52d55ea5badb0081c9c5f87
Marcel Veselka <marcel.veselka@tesena.com>
2022-05-07 20:55:10 +0100

    debug

--- install.sh
+++ install.sh
@@ -1,21 +1,25 @@
 
 
-if [ $# -lt 4 ] ;then
-    echo "Usage: <github token> <org/repo> <filename> <version or 'latest'>"
-    exit 1
-fi
+# if [ $# -lt 4 ] ;then
+#     echo "Usage: <github token> <org/repo> <filename> <version or 'latest'>"
+#     exit 1
+# fi
 
-TOKEN="$1"
-REPO="$2"
-FILE="$3"      # the name of your release asset file, e.g. build.tar.gz
-VERSION=$4     # tag name or the word "latest"
+# TOKEN="$1"
+# REPO="$2"
+# FILE="$3"      # the name of your release asset file, e.g. build.tar.gz
+# VERSION=$4     # tag name or the word "latest"
 
-# https://github.com/autonomous-testing/bot-mvp/releases/download/0.1.2/bot_mvp-0.1.2-py3-none-any.whl
-# curl -L -s -H "Authorization: token $TOKEN" -H 'Accept:application/octet-stream' \
-# -H "Authorization: token $TOKEN" 
-# -H 'Accept:application/octet-stream' \
+# # https://github.com/autonomous-testing/bot-mvp/releases/download/0.1.2/bot_mvp-0.1.2-py3-none-any.whl
+# # curl -L -s -H "Authorization: token $TOKEN" -H 'Accept:application/octet-stream' \
+# # -H "Authorization: token $TOKEN" 
+# # -H 'Accept:application/octet-stream' \
+# curl \
+# -L -s \
+# -H "Accept: application/vnd.github.v3+json" \
+# "https://api.github.com/repos/$REPO/releases/assets/0.1.2" \
+# -o $FILE
+
 curl \
--L -s \
--H "Accept: application/vnd.github.v3+json" \
-"https://api.github.com/repos/$REPO/releases/assets/0.1.2" \
--o $FILE
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/autonomous-testing/bot-mvp/releases/assets/0.1.2
