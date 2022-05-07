1c3de44b83525ac4a19ed862f31a2d7e3e65444f
Marcel Veselka <marcel.veselka@tesena.com>
2022-05-07 20:46:22 +0100

    debug

--- install.sh
+++ install.sh
@@ -11,6 +11,11 @@ FILE="$3"      # the name of your release asset file, e.g. build.tar.gz
 VERSION=$4     # tag name or the word "latest"
 
 # https://github.com/autonomous-testing/bot-mvp/releases/download/0.1.2/bot_mvp-0.1.2-py3-none-any.whl
-curl -L -s -H "Authorization: token $TOKEN" -H 'Accept:application/octet-stream' \
+# curl -L -s -H "Authorization: token $TOKEN" -H 'Accept:application/octet-stream' \
+# -H "Authorization: token $TOKEN" 
+# -H 'Accept:application/octet-stream' \
+curl \
+-L -s \
+-H "Accept: application/vnd.github.v3+json" \
 "https://api.github.com/repos/$REPO/releases/assets/0.1.2" \
 -o $FILE
